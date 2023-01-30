a1fb3791e3f594b6a961b33d1a11eda51ddf6f21
Hossein Araby <hosseiw@yahoo.com>
2023-01-30 08:46:48 +0330

    update

--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -1,9 +1,10 @@
 import Header from "../../components/header/Header";
+import Menu from "../../components/menu/Menu";
 
 const App = () => {
   return (
-    <div className="bg-[#101010] w-full h-[256px] ">
-      <Header />
+    <div className="bg-[#101010] w-full h-[256px] flex align-middle items-center mx-auto ">
+      {/* <Header /> */}
       {/* <div>
         <div className="w-full h-full">
           <h1>Btn SCHEME </h1>
@@ -15,6 +16,8 @@ const App = () => {
           </div>
         </div>
       </div> */}
+
+      <Menu />
     </div>
   );
 };
